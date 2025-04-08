9213820e9f6ecbffaeaf6b4c1517220daad26094
camou92 <cmohamed992@gmail.com>
2025-04-08 05:10:43 +0100

    add project

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,18 +1,33 @@
 import { useState } from "react";
 import ecom from "../assets/ecom.png";
-import ecommerce from "../assets/e-commerce-app.png";
-import food from "../assets/food-app.jpg";
-import travel from "../assets/travel-app.jpg";
+import ecomapi from "../assets/ecomapi.png";
+import cv from "../assets/cv.png";
 import RevealOnScroll from "../Ui/RevealOnScroll";
 
 function Projects() {
+  const [nestjs] = useState([
+    {
+      image: cv,
+      name: "Gestion Cv",
+      brief:
+        "Gestion de cv, CRUD complet authentification, authorisation gestion des roles, guards",
+    },
+  ]);
+
+  const [postgres] = useState([
+    {
+      image: ecomapi,
+      name: "E-commerce Api",
+      brief: "Ecommerce api nestjs",
+    },
+  ]);
+
   const [projects] = useState([
     {
       image: ecom,
       name: "E-commerce App",
-      brief:
-        "Développement d'un site ecommerce",
-    }
+      brief: "Développement d'un site ecommerce",
+    },
   ]);
   return (
     <section
@@ -26,6 +41,70 @@ function Projects() {
           </h2>
 
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            {nestjs.map((project, index) => (
+              <div
+                key={index}
+                className="p-6 rounded-xl flex flex-col items-center text-center border-2 border-white/10 hover:border-blue-500/30 hover:shadow-[9_2px_8px_rgba(59,130,246,0.2)] hover:-translate-y-1 transition-all hover:bg-cyan-900 "
+              >
+                <h3 className="font-semibold text-xl mb-4">{project.name}</h3>
+                <p className="text-gray-300 mb-4">{project.brief} </p>
+                <div className="flex flex-wrap sm:items-center gap-2 mb-4">
+                  {["NestJs", "TypeOrm", "Mysql", "Docker"].map(
+                    (tech, index) => (
+                      <span
+                        key={index}
+                        className="bg-blue-500/10 text-blue-400 py-1 px-3 rounded-full text-balance hover:bg-blue-500/20 hover:shadow-[9_2px_8px_rgba(59,130,22.46,0.2)] transition "
+                      >
+                        {tech}
+                      </span>
+                    )
+                  )}
+                  <div className="flex flex-col text-center my-5 py-3 justify-center gap-3 border-2 border-white/10 hover:border-blue-500/30 hover:shadow-[9_2px_8px_rgba(59,130,22.46,0.2)]  cursor-pointer rounded-xl ">
+                    <p className="text-gray-200">View Project</p>
+                    <a
+                      href="https://github.com/camou92/gestion_cv_nestjs.git"
+                      target="blank"
+                      className="flex justify-between items-center font-bold text-blue-400 transition-colors hover:text-white"
+                    >
+                      {" "}
+                      <img src={project.image} alt={`${project.name}Image `} />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ))}
+            {postgres.map((project, index) => (
+              <div
+                key={index}
+                className="p-6 rounded-xl flex flex-col items-center text-center border-2 border-white/10 hover:border-blue-500/30 hover:shadow-[9_2px_8px_rgba(59,130,246,0.2)] hover:-translate-y-1 transition-all hover:bg-cyan-900 "
+              >
+                <h3 className="font-semibold text-xl mb-4">{project.name}</h3>
+                <p className="text-gray-300 mb-4">{project.brief} </p>
+                <div className="flex flex-wrap sm:items-center gap-2 mb-4">
+                  {["NestJs", "TypeOrm", "Postgresql", "Docker"].map(
+                    (tech, index) => (
+                      <span
+                        key={index}
+                        className="bg-blue-500/10 text-blue-400 py-1 px-3 rounded-full text-balance hover:bg-blue-500/20 hover:shadow-[9_2px_8px_rgba(59,130,22.46,0.2)] transition "
+                      >
+                        {tech}
+                      </span>
+                    )
+                  )}
+                  <div className="flex flex-col text-center my-5 py-3 justify-center gap-3 border-2 border-white/10 hover:border-blue-500/30 hover:shadow-[9_2px_8px_rgba(59,130,22.46,0.2)]  cursor-pointer rounded-xl ">
+                    <p className="text-gray-200">View Project</p>
+                    <a
+                      href="https://github.com/camou92/ecommerce_api_nestjs.git"
+                      target="blank"
+                      className="flex justify-between items-center font-bold text-blue-400 transition-colors hover:text-white"
+                    >
+                      {" "}
+                      <img src={project.image} alt={`${project.name}Image `} />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            ))}
             {projects.map((project, index) => (
               <div
                 key={index}
@@ -34,7 +113,7 @@ function Projects() {
                 <h3 className="font-semibold text-xl mb-4">{project.name}</h3>
                 <p className="text-gray-300 mb-4">{project.brief} </p>
                 <div className="flex flex-wrap sm:items-center gap-2 mb-4">
-                  {["Wordpress",].map((tech, index) => (
+                  {["Wordpress"].map((tech, index) => (
                     <span
                       key={index}
                       className="bg-blue-500/10 text-blue-400 py-1 px-3 rounded-full text-balance hover:bg-blue-500/20 hover:shadow-[9_2px_8px_rgba(59,130,22.46,0.2)] transition "
@@ -45,7 +124,8 @@ function Projects() {
                   <div className="flex flex-col text-center my-5 py-3 justify-center gap-3 border-2 border-white/10 hover:border-blue-500/30 hover:shadow-[9_2px_8px_rgba(59,130,22.46,0.2)]  cursor-pointer rounded-xl ">
                     <p className="text-gray-200">View Project</p>
                     <a
-                      href="https://mkfashionworld.com/" target="blank"
+                      href="https://mkfashionworld.com/"
+                      target="blank"
                       className="flex justify-between items-center font-bold text-blue-400 transition-colors hover:text-white"
                     >
                       {" "}
